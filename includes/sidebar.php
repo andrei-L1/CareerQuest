e9f64d583ac749d0728ba447aa1701fd13e2fc37
andrei-L1 <adr.lonsania@gmail.com>
2025-03-05 14:50:08 +0800

    commit

---
+++ includes/sidebar.php
@@ -0,0 +1,11 @@
+<?php
+$sidebar_menu = [
+    ["Dashboard", "fas fa-tachometer-alt", "#"],
+    ["User Management", "fas fa-users", "user-management.html"],
+    ["Job Postings", "fas fa-briefcase", "#"],
+    ["Analytics", "fas fa-chart-line", "#"],
+    ["Forum Moderation", "fas fa-comments", "#"],
+    ["Settings", "fas fa-cog", "#"]
+];
+?>
+
