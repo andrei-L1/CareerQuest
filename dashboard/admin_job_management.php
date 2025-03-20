3a5cd31a0499547316454cb476edb40dcab9e4c7
andrei-L1 <adr.lonsania@gmail.com>
2025-03-20 09:40:51 +0800

    commit

---
+++ dashboard/admin_job_management.php
@@ -0,0 +1,8 @@
+<?php
+include "../includes/sidebar.php";
+require "../controllers/chart_query.php";
+require "../controllers/admin_dashboard.php";
+require "../controllers/admin_user_management.php";
+require '../auth/auth_check.php'; 
+?>
+
