fab539d7d277acd46d2fad4d1d87deccb9e672f7
andrei-L1 <adr.lonsania@gmail.com>
2025-03-27 14:47:23 +0800

    commit

--- includes/stud_navbar.php
+++ includes/stud_navbar.php
@@ -445,7 +445,7 @@ $lastName = htmlspecialchars($studentData['stud_last_name'] ?? '');
 <nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top">
     <div class="container">
         <a class="navbar-brand fw-bold" href="../dashboard/student.php">
-            CareerConnect
+            CareerQuest
         </a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
