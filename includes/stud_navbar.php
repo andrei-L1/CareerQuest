f35a1c7b642c5408eaf7cccf5a2d64036b429b53
andrei-L1 <adr.lonsania@gmail.com>
2025-04-10 14:42:04 +0800

    commit

--- includes/stud_navbar.php
+++ includes/stud_navbar.php
@@ -76,7 +76,7 @@ $nav_links = [
     "Applications" => "applications.php",
     "Forum" => "forums.php",
     "Messages" => "messages.php",
-    "Notifications" => "notifications.php"
+    "Notifications" => "stud_notification.php"
 ];
 
 // Get student name safely
