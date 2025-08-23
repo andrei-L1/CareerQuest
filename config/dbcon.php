848692cc0ba1bd07f000e03eeade948f64f5ed49
andrei-L1 <adr.lonsania@gmail.com>
2025-08-23 17:16:05 +0800

    Added database connection into the hosting database

--- config/dbcon.php
+++ config/dbcon.php
@@ -12,4 +12,18 @@ try {
     die("Database connection failed: " . $e->getMessage());
 }
 
+/*
+$host = 'sql104.byethost9.com';
+$dbname = 'b9_39771656_career_platform';
+$username = 'b9_39771656';
+$password = 'FreeDomainYey123';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+    
+*/
 ?>
